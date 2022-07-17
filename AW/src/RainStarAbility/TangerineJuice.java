74914f6762fe39b61372a10382444630e3d8343e
RainStar <RainStar@W23088>
2022-07-17 16:56:02 +0900

    R v1.5.0(PreRelease)

--- AW/src/RainStarAbility/TangerineJuice.java
+++ AW/src/RainStarAbility/TangerineJuice.java
@@ -8,23 +8,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.Map.Entry;
 
+import javax.annotation.Nullable;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.util.Vector;
 
-import RainStarAbility.RainStar.Grab;
+import daybreak.abilitywar.AbilityWar;
 import daybreak.abilitywar.ability.AbilityBase;
 import daybreak.abilitywar.ability.AbilityManifest;
 import daybreak.abilitywar.ability.SubscribeEvent;
@@ -34,26 +41,31 @@ import daybreak.abilitywar.config.ability.AbilitySettings.SettingObject;
 import daybreak.abilitywar.game.GameManager;
 import daybreak.abilitywar.game.AbstractGame.Participant;
 import daybreak.abilitywar.game.AbstractGame.Participant.ActionbarNotification.ActionbarChannel;
+import daybreak.abilitywar.game.module.DeathManager;
 import daybreak.abilitywar.game.module.Wreck;
+import daybreak.abilitywar.game.team.interfaces.Teamable;
 import daybreak.abilitywar.utils.base.color.RGB;
 import daybreak.abilitywar.utils.base.concurrent.TimeUnit;
 import daybreak.abilitywar.utils.base.math.LocationUtil;
+import daybreak.abilitywar.utils.base.math.VectorUtil;
 import daybreak.abilitywar.utils.base.minecraft.block.Blocks;
 import daybreak.abilitywar.utils.base.minecraft.block.IBlockSnapshot;
-import daybreak.abilitywar.utils.base.minecraft.damage.Damages;
 import daybreak.abilitywar.utils.base.minecraft.nms.NMS;
 import daybreak.abilitywar.utils.base.random.Random;
 import daybreak.abilitywar.utils.library.BlockX;
 import daybreak.abilitywar.utils.library.MaterialX;
 import daybreak.abilitywar.utils.library.ParticleLib;
+import daybreak.abilitywar.utils.library.SoundLib;
+import daybreak.google.common.base.Predicate;
 import daybreak.google.common.base.Strings;
 
 @AbilityManifest(name = "귤즙", rank = Rank.S, species = Species.HUMAN, explain = {
 		"§7게이지 §8- §e과즙§f: $[PERIOD]초마다 한 칸씩 최대 10칸을 보유 가능합니다.", 
-		"§7화살 발사 §8- §6귤 화살§f: 적중 위치에 과즙 장판이 터집니다. $[ARROW_CONSUME]",
+		"§7화살 발사 §8- §6귤 화살§f: 적중 위치에 §a과즙 장판§f이 터집니다. $[ARROW_CONSUME]",
 		" 만일 생명체를 적중시킨다면 $[GAUGE_RETURN]칸만큼 §e과즙 게이지§f를 돌려받습니다.",
-		" 장판 위의 적은 §3끌리거나 밀리는§8(§7벡터§8)§f 효과가 대폭 감소합니다.",
-		" 장판 위 적에게 주는 §b원거리 피해§f가 $[PROJECTILE_DAMAGE_INCREASE]% 증가합니다.",
+		" §a장판§f 위의 적은 중심으로 끌려가며 이외 §3끌리거나 밀리는§8(§7벡터§8)§f 효과가 감소합니다.",
+		" §a장판§f 위 적에게 주는 §b원거리 피해§f가 $[LONG_DISTANCE_DAMAGE_INCREASE]% 증가합니다.",
+		" §a장판§f의 범위는 $[FIELD_RANGE]칸, 지속은 $[FIELD_DURATION]초 유지됩니다.",
 		"§7철괴 우클릭 §8- §c과즙 폭발§f: 모든 과즙 게이지를 전부 소모하여 제자리에 터뜨려",
 		" $[RANGE]칸 내의 적을 강하게 밀쳐내고 §7실명§f시킵니다. 이때 잠시 빨라집니다.",
 		" 모든 효과§8(§7넉백, 실명, 신속§8)§f의 세기는 게이지에 비례합니다.",
@@ -104,8 +116,8 @@ public class TangerineJuice extends AbilityBase {
 
 	};
 	
-	public static final SettingObject<Integer> PROJECTILE_DAMAGE_INCREASE = 
-			abilitySettings.new SettingObject<Integer>(TangerineJuice.class, "projectile-damage-increase", 25,
+	public static final SettingObject<Integer> LONG_DISTANCE_DAMAGE_INCREASE = 
+			abilitySettings.new SettingObject<Integer>(TangerineJuice.class, "long-distance-damage-increase", 25,
 			"# 장판 위 적에게 주는 추가 대미지") {
 
 		@Override
@@ -137,27 +149,76 @@ public class TangerineJuice extends AbilityBase {
 
 	};
 	
-	private final Map<Block, IBlockSnapshot> blockData = new HashMap<>();
-	private final Set<Block> notchangedblocks = new HashSet<>();
-	private final int returngauge = GAUGE_RETURN.getValue();
+	public static final SettingObject<Integer> FIELD_DURATION = 
+			abilitySettings.new SettingObject<Integer>(TangerineJuice.class, "field-duration", 10,
+			"# 과즙 장판 지속시간", "# 단위: 초") {
+
+		@Override
+		public boolean condition(Integer value) {
+			return value >= 0;
+		}
+
+	};
+	
+	private final Predicate<Entity> predicate = new Predicate<Entity>() {
+		@Override
+		public boolean test(Entity entity) {
+			if (entity instanceof Player) {
+				if (!getGame().isParticipating(entity.getUniqueId())
+						|| (getGame() instanceof DeathManager.Handler && ((DeathManager.Handler) getGame())
+								.getDeathManager().isExcluded(entity.getUniqueId()))
+						|| !getGame().getParticipant(entity.getUniqueId()).attributes().TARGETABLE.getValue()) {
+					return false;
+				}
+				if (getGame() instanceof Teamable) {
+					final Teamable teamGame = (Teamable) getGame();
+					final Participant entityParticipant = teamGame.getParticipant(entity.getUniqueId()),
+							participant = getParticipant();
+					return !teamGame.hasTeam(entityParticipant) || !teamGame.hasTeam(participant)
+							|| (!teamGame.getTeam(entityParticipant).equals(teamGame.getTeam(participant)));
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public boolean apply(@Nullable Entity arg0) {
+			return false;
+		}
+	};
+	
+	private final int period = (int) Math.ceil(Wreck.isEnabled(GameManager.getGame()) ? Wreck.calculateDecreasedAmount(60) * (PERIOD.getValue() * 20) : (PERIOD.getValue() * 20));
 	private final int consume = ARROW_CONSUME.getValue();
+	private final int returngauge = GAUGE_RETURN.getValue();
+	private final double dmgIncrease = 1 + (LONG_DISTANCE_DAMAGE_INCREASE.getValue() * 0.01);
 	private final int fieldrange = FIELD_RANGE.getValue();
+	private final int fieldduration = FIELD_DURATION.getValue() * 5;
+	private final Set<Vector> vectors = new HashSet<>();
+	
 	private ActionbarChannel ac = newActionbarChannel();
 	private int juicegauge = 0;
 	
-	private final int period = (int) Math.ceil(Wreck.isEnabled(GameManager.getGame()) ? Wreck.calculateDecreasedAmount(60) * (PERIOD.getValue() * 20) : (PERIOD.getValue() * 20));
-	
 	private Map<Projectile, ArrowParticle> arrowParticles = new HashMap<>();
+	
+	@Override
+	public void onUpdate(Update update) {
+		if (update == Update.RESTRICTION_CLEAR) juice.start();
+	}
 	
 	private AbilityTimer juice = new AbilityTimer() {
 		
 		@Override
 		public void run(int count) {
-			if (period == 0) juicegauge = 10;
-			else if (count % period == 0) juicegauge = Math.min(10, juicegauge + 1);
+			if (period == 0 && juicegauge < 10) {
+				SoundLib.ITEM_BUCKET_FILL.playSound(getPlayer().getLocation(), 1, 2);
+				ParticleLib.DRIP_LAVA.spawnParticle(getPlayer().getLocation(), 0.5, 1, 0.5, 7, 1);
+				juicegauge = 10;
+			} else if (count % period == 0) {
+				SoundLib.ITEM_BUCKET_FILL.playSound(getPlayer().getLocation(), 1, 2);
+				ParticleLib.DRIP_LAVA.spawnParticle(getPlayer().getLocation(), 0.5, 1, 0.5, 7, 1);
+				juicegauge = Math.min(10, juicegauge + 1);
+			}
 			ac.update(Strings.repeat("§7/", 10 - juicegauge) + Strings.repeat("§6/", juicegauge));
-			
-			
 		}
 		
 	}.setPeriod(TimeUnit.TICKS, 1).register();
@@ -179,60 +240,121 @@ public class TangerineJuice extends AbilityBase {
     		if (e.getHitEntity() != null) juicegauge = Math.min(10, juicegauge + returngauge);
     		ParticleLib.EXPLOSION_LARGE.spawnParticle(e.getEntity().getLocation());
     		e.getEntity().getWorld().createExplosion(e.getEntity().getLocation(), 1.2f, false, false);
-    		
-			for (int ranges = 0; ranges < fieldrange; ranges++) {
-				for (Block block : LocationUtil.getBlocks2D(e.getEntity().getLocation(), ranges, true, true, true)) {
+    		new Field(fieldduration, e.getEntity().getLocation()).start();
+    	}
+    }
+    
+	public class Field extends AbilityTimer implements Listener {
+		
+		private final Location center;
+		private final Map<Block, IBlockSnapshot> blockData = new HashMap<>();
+		private final Set<Block> notchangedblocks = new HashSet<>();
+		
+		public Field(int duration, Location center) {
+			super(TaskType.NORMAL, duration);
+			setPeriod(TimeUnit.TICKS, 4);
+			this.center = center;
+		}
+		
+		@Override
+		public void onStart() {
+			Bukkit.getPluginManager().registerEvents(this, AbilityWar.getPlugin());
+		}
+		
+		@Override
+		public void run(int count) {
+			if (count <= fieldrange) {
+				for (Block block : LocationUtil.getBlocks2D(center, count, true, true, true)) {
 					Block belowBlock = block.getRelative(BlockFace.DOWN);
 					if (MaterialX.ORANGE_CONCRETE.compare(belowBlock)) {
 						block = belowBlock;
 						belowBlock = belowBlock.getRelative(BlockFace.DOWN);
 						notchangedblocks.add(belowBlock);
-					} else blockData.putIfAbsent(belowBlock, Blocks.createSnapshot(belowBlock));
+					} else {
+						blockData.putIfAbsent(belowBlock, Blocks.createSnapshot(belowBlock));
+					}
 					BlockX.setType(belowBlock, MaterialX.ORANGE_CONCRETE);
 				}
 			}
-    	}
-    }
-    
-    @SubscribeEvent
-    public void onEntityDamageByEvent(EntityDamageByEntityEvent e) {
-		Player damager = null;
-		if (e.getDamager() instanceof Projectile) {
-			Projectile projectile = (Projectile) e.getDamager();
-			if (projectile.getShooter() instanceof Player) damager = (Player) projectile.getShooter();
-		} else if (e.getDamager() instanceof Player) damager = (Player) e.getDamager();
-		
-		if (getPlayer().equals(damager) && !getPlayer().equals(e.getEntity())) {
-			Block b = LocationUtil.floorY(e.getEntity().getLocation()).getBlock();
-			Block bb = LocationUtil.floorY(e.getEntity().getLocation()).clone().subtract(0, 1, 0).getBlock();
-			if (blockData.containsKey(b) || blockData.containsKey(bb) || notchangedblocks.contains(b) || notchangedblocks.contains(bb)) {
-				final double distance = Math.min(10, getPlayer().getLocation().distance(e.getEntity().getLocation()));
-				if (distance >= 5) {
-					
-				}
+			
+			for (Player players : LocationUtil.getEntitiesInCircle(Player.class, center, fieldrange, predicate)) {
+				Vector vector = VectorUtil.validateVector(center.toVector().subtract(players.getLocation().toVector()).normalize().setY(0).multiply(0.2));
+				vectors.add(vector);
+				players.setVelocity(vector);
 			}
 		}
-    }
-    
-	@SubscribeEvent
-	public void onBlockBreak(BlockBreakEvent e) {
-		if (blockData.containsKey(e.getBlock()) || notchangedblocks.contains(e.getBlock())) {
-			e.setCancelled(true);
+		
+		@Override
+		public void onEnd() {
+			onSilentEnd();
 		}
-	}
+		
+		@Override
+		public void onSilentEnd() {
+			HandlerList.unregisterAll(this);
+			for (Entry<Block, IBlockSnapshot> entry : blockData.entrySet()) {
+				Block key = entry.getKey();
+				if (MaterialX.ORANGE_CONCRETE.compare(key)) {
+					entry.getValue().apply();
+				}
+			}
+			blockData.clear();
+			notchangedblocks.clear();
+		}
+		
+		@EventHandler()
+		public void onBlockBreak(BlockBreakEvent e) {
+			if (blockData.containsKey(e.getBlock()) || notchangedblocks.contains(e.getBlock())) {
+				e.setCancelled(true);
+			}
+		}
 
-	@SubscribeEvent
-	public void onExplode(BlockExplodeEvent e) {
-		e.blockList().removeIf(blockData::containsKey);
-		e.blockList().removeIf(notchangedblocks::contains);
-	}
+		@EventHandler()
+		public void onExplode(BlockExplodeEvent e) {
+			e.blockList().removeIf(blockData::containsKey);
+			e.blockList().removeIf(notchangedblocks::contains);
+		}
 
-	@SubscribeEvent
-	public void onExplode(EntityExplodeEvent e) {
-		e.blockList().removeIf(blockData::containsKey);
-		e.blockList().removeIf(notchangedblocks::contains);
+		@EventHandler()
+		public void onExplode(EntityExplodeEvent e) {
+			e.blockList().removeIf(blockData::containsKey);
+			e.blockList().removeIf(notchangedblocks::contains);
+		}
+		
+		@EventHandler()
+		private void onVelocity(PlayerVelocityEvent e) {
+			if (predicate.test(e.getPlayer())) {
+				Block b = LocationUtil.floorY(e.getPlayer().getLocation()).getBlock();
+				Block bm = LocationUtil.floorY(e.getPlayer().getLocation()).clone().subtract(0, 1, 0).getBlock();
+				if (blockData.containsKey(b) || blockData.containsKey(bm) || notchangedblocks.contains(b) || notchangedblocks.contains(bm)) {
+					if (!vectors.contains(e.getVelocity())) e.setVelocity(e.getVelocity().multiply(0.25));
+					else vectors.remove(e.getVelocity());
+				}	
+			}
+		}
+		
+	    @EventHandler()
+	    public void onEntityDamageByEvent(EntityDamageByEntityEvent e) {
+			Player damager = null;
+			if (e.getDamager() instanceof Projectile) {
+				Projectile projectile = (Projectile) e.getDamager();
+				if (projectile.getShooter() instanceof Player) damager = (Player) projectile.getShooter();
+			} else if (e.getDamager() instanceof Player) damager = (Player) e.getDamager();
+			
+			if (getPlayer().equals(damager) && !getPlayer().equals(e.getEntity())) {
+				Block b = LocationUtil.floorY(e.getEntity().getLocation()).getBlock();
+				Block bm = LocationUtil.floorY(e.getEntity().getLocation()).clone().subtract(0, 1, 0).getBlock();
+				if (blockData.containsKey(b) || blockData.containsKey(bm) || notchangedblocks.contains(b) || notchangedblocks.contains(bm)) {
+					final double distance = Math.min(10, getPlayer().getLocation().distance(e.getEntity().getLocation()));
+					if (distance >= 5) {
+						e.setDamage(e.getDamage() * dmgIncrease);
+					}
+				}
+			}
+	    }
+		
 	}
-    
+	
     public class ArrowParticle extends AbilityTimer {
     	
 		private Location lastloc;
