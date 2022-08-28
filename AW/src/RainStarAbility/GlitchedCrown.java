78dc79bd6df0e38a1b4a18a544b23426576b0115
RainStar <RainStar@W23088>
2022-08-28 14:10:16 +0900

    R v1.5.4(PreRelease)
    버전 되돌림
    서큐버스 작업 시작
    호루스의 대미지 중첩이 선형적으로 감소함
    귤즙 액티브 넉백 증가

--- AW/src/RainStarAbility/GlitchedCrown.java
+++ AW/src/RainStarAbility/GlitchedCrown.java
@@ -1,5 +1,12 @@
 package RainStarAbility;
 
-public class GlitchedCrown {
+import daybreak.abilitywar.ability.AbilityBase;
+import daybreak.abilitywar.game.AbstractGame.Participant;
+
+public class GlitchedCrown extends AbilityBase {
+	
+	public GlitchedCrown(Participant participant) {
+		super(participant);
+	}
 
 }
