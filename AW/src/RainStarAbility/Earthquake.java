626c84bbadbd7c5dae793e40396add1e22132b45
RainStar <RainStar@W23088>
2022-09-18 16:48:45 +0900

    R v1.6.3(Beta)

--- AW/src/RainStarAbility/Earthquake.java
+++ AW/src/RainStarAbility/Earthquake.java
@@ -2,12 +2,17 @@ package RainStarAbility;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nullable;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -24,6 +29,8 @@ import daybreak.abilitywar.game.manager.effect.Stun;
 import daybreak.abilitywar.game.module.DeathManager;
 import daybreak.abilitywar.game.team.interfaces.Teamable;
 import daybreak.abilitywar.utils.base.Formatter;
+import daybreak.abilitywar.utils.base.color.Gradient;
+import daybreak.abilitywar.utils.base.color.RGB;
 import daybreak.abilitywar.utils.base.concurrent.TimeUnit;
 import daybreak.abilitywar.utils.base.math.LocationUtil;
 import daybreak.abilitywar.utils.base.math.geometry.Circle;
@@ -71,7 +78,7 @@ public class Earthquake extends AbilityBase implements ActiveHandler {
 	};
 	
 	public static final SettingObject<Double> STUN = 
-			abilitySettings.new SettingObject<Double>(Earthquake.class, "stun", 3.5,
+			abilitySettings.new SettingObject<Double>(Earthquake.class, "stun", 4.0,
 			"# 기절 시간") {
 
 		@Override
@@ -126,35 +133,36 @@ public class Earthquake extends AbilityBase implements ActiveHandler {
 	};
 	
 	private final Random random = new Random();
-	private final double min = RANGE_MIN.getValue();
-	private final double max = RANGE_MAX.getValue();
 	private final int stun = (int) (STUN.getValue() * 20);
 	private final Cooldown cooldown = new Cooldown(COOLDOWN.getValue());
 	private final Map<Player, Airborn> airborned = new HashMap<>();
-	private Vector upper;
 	private final DecimalFormat df = new DecimalFormat("0.000");
 	private Circle circle;
 	
 	public boolean ActiveSkill(Material material, ClickType clickType) {
-		if (material == Material.IRON_INGOT && clickType == ClickType.RIGHT_CLICK && !cooldown.isCooldown()) {
-			double range = (random.nextDouble() * (max - min)) + min;
-			circle = Circle.of(range, (int) (range * 15));
-			getPlayer().sendMessage("§e진도 범위§f: " + df.format(range));
-			upper = new Vector(0, (range * 0.07), 0);
-			for (LivingEntity livingEntity : LocationUtil.getEntitiesInCircle(LivingEntity.class, getPlayer().getLocation(), range, predicate)) {
-				if (livingEntity.isOnGround()) {
-					livingEntity.setVelocity(upper);
-					if (livingEntity instanceof Player) new Airborn((Player) livingEntity).start();
-				}
-			}
-			for (Location loc : circle.toLocations(getPlayer().getLocation()).floor(getPlayer().getLocation().getY())) {
-				ParticleLib.CLOUD.spawnParticle(getPlayer(), loc, 0, 0, 0, 1, 0);
-			}
-			SoundLib.ENTITY_GENERIC_EXPLODE.playSound(getPlayer().getLocation(), (float) (range / 5), 1);
-			return cooldown.start();
+		if (material == Material.IRON_INGOT && clickType == ClickType.RIGHT_CLICK && !cooldown.isCooldown() && !earthquakeready.isRunning()) {
+			return earthquakeready.start();
 		}
 		return false;
 	}
+	
+	public AbilityTimer earthquakeready = new AbilityTimer() {
+		
+		double richter;
+		
+		@Override
+		public void onStart() {
+			richter = (random.nextInt(96) + 5) * 0.1;
+			Bukkit.broadcastMessage("§4[§c!§4] §d" + getPlayer().getLocation().getX() + "");
+			SoundLib.ENTITY_GENERIC_EXPLODE.playSound(getPlayer().getLocation(), (float) (richter / 2), 1);
+		}
+		
+		@Override
+		public void run(int count) {
+			
+		}
+		
+	}.setPeriod(TimeUnit.TICKS, 1).register();
 	
 	@SuppressWarnings("deprecation")
 	public class Airborn extends AbilityTimer {
@@ -177,6 +185,85 @@ public class Earthquake extends AbilityBase implements ActiveHandler {
 		public void onEnd() {
 			airborned.remove(player);
 			Stun.apply(getGame().getParticipant(player), TimeUnit.TICKS, stun);
+		}
+		
+	}
+	
+	public class EarthquakeWave extends AbilityTimer {
+
+		private double radius;
+		private final Predicate<Entity> predicate;
+		private final Location center;
+		private final int maxCount;
+		
+		private RGB startColor = RGB.of(200, 1, 1), endColor = RGB.of(30, 1, 1);
+		private final List<RGB> gradations;
+		
+		public EarthquakeWave(int count, Location center) {
+			super(TaskType.NORMAL, count);
+			setPeriod(TimeUnit.TICKS, 1);
+			this.gradations = Gradient.createGradient(count, startColor, endColor);
+			this.center = center;
+			this.maxCount = count;
+			this.predicate = new Predicate<Entity>() {
+				@Override
+				public boolean test(Entity entity) {
+					if (!entity.isOnGround()) return false;
+					if (entity instanceof ArmorStand) return false;
+					if (entity.equals(getPlayer())) return false;
+					if (entity instanceof Player) {
+						if (!getGame().isParticipating(entity.getUniqueId())
+								|| (getGame() instanceof DeathManager.Handler && ((DeathManager.Handler) getGame()).getDeathManager().isExcluded(entity.getUniqueId()))
+								|| !getGame().getParticipant(entity.getUniqueId()).attributes().TARGETABLE.getValue()) {
+							return false;
+						}
+						if (getGame() instanceof Teamable) {
+							final Teamable teamGame = (Teamable) getGame();
+							final Participant entityParticipant = teamGame.getParticipant(entity.getUniqueId()), participant = teamGame.getParticipant(getPlayer().getUniqueId());
+							if (participant != null) {
+								return !teamGame.hasTeam(entityParticipant) || !teamGame.hasTeam(participant) || (!teamGame.getTeam(entityParticipant).equals(teamGame.getTeam(participant)));
+							}
+						}
+					}
+					return true;
+				}
+
+				@Override
+				public boolean apply(@Nullable Entity arg0) {
+					return false;
+				}
+			};
+		}
+		
+		@Override
+		public void onStart() {
+			
+		}
+		
+		@Override
+		public void run(int count) {
+			radius += 0.3;
+			for (Iterator<Location> iterator = Circle.iteratorOf(center, radius, maxCount); iterator.hasNext(); ) {
+				Location loc = iterator.next();
+				LocationUtil.floorY(loc);
+				ParticleLib.CRIT.spawnParticle(loc, 0, 0, 0, 1, 0.35);
+				ParticleLib.REDSTONE.spawnParticle(loc, gradations.get(count));
+				SoundLib.BLOCK_STONE_HIT.playSound(loc, 0.5f, 1);
+				for (Damageable damageable : LocationUtil.getNearbyEntities(Damageable.class, loc, 0.6, 0.6, predicate)) {
+					damageable.setVelocity(new Vector(0, maxCount / 100.0, 0));
+					if (damageable instanceof Player) new Airborn((Player) damageable).start();
+				}
+			}
+		}
+		
+		@Override
+		public void onEnd() {
+			onSilentEnd();
+		}
+		
+		@Override
+		public void onSilentEnd() {
+			
 		}
 		
 	}
