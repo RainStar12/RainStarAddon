4492b31d672ada37ed96af0b87d2fb3f26648299
RainStar <RainStar@W23088>
2022-11-06 23:27:38 +0900

    R v1.7.0(Beta)

--- AW/src/rainstar/aw/KillRewardGUI.java
+++ AW/src/rainstar/aw/KillRewardGUI.java
@@ -1,5 +1,15 @@
 package rainstar.aw;
 
-public class KillRewardGUI {
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import daybreak.abilitywar.config.wizard.SettingWizard;
+
+public class KillRewardGUI extends SettingWizard {
+
+	KillRewardGUI(Player player, int inventorySize, String inventoryName, Plugin plugin) {
+		super(player, inventorySize, inventoryName, plugin);
+		// TODO Auto-generated constructor stub
+	}
 
 }
