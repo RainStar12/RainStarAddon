7bbc194d23408299e452e972842238cfb0e05fec
RainStar <RainStar@W22959>
2023-02-26 23:50:04 +0900

    R v1.8.5(Beta)

--- AW/src/rainstar/abilitywar/ability/Buff.java
+++ AW/src/rainstar/abilitywar/ability/Buff.java
@@ -320,7 +320,7 @@ public class Buff extends AbilityBase implements ActiveHandler {
 			final EntityRegainHealthEvent event = new EntityRegainHealthEvent(getPlayer(), healamount, RegainReason.CUSTOM);
 			Bukkit.getPluginManager().callEvent(event);
 			if (!event.isCancelled()) {
-				Healths.setHealth(getPlayer(), getPlayer().getHealth() + healamount);	
+				Healths.setHealth(getPlayer(), getPlayer().getHealth() + event.getAmount());	
 			}
 			cool.start();
 		}
