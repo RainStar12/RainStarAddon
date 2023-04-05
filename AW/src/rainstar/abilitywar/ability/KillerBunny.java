4e504dfee8cfc30097b4dc75572bbd05304aeb0a
RainStar <RainStar@W23088>
2023-04-05 23:56:34 +0900

    R v1.9.1(Beta)

--- AW/src/rainstar/abilitywar/ability/KillerBunny.java
+++ AW/src/rainstar/abilitywar/ability/KillerBunny.java
@@ -1,14 +1,20 @@
 package rainstar.abilitywar.ability;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
 import daybreak.abilitywar.ability.AbilityBase;
 import daybreak.abilitywar.ability.AbilityManifest;
 import daybreak.abilitywar.ability.AbilityManifest.Rank;
 import daybreak.abilitywar.ability.AbilityManifest.Species;
+import daybreak.abilitywar.ability.SubscribeEvent;
 import daybreak.abilitywar.config.ability.AbilitySettings.SettingObject;
 import daybreak.abilitywar.game.AbstractGame.Participant;
 
 @AbilityManifest(name = "살인마 토끼", rank = Rank.L, species = Species.ANIMAL, explain = {
-		"§7패시브 §8- §c살의§f: 모든 공격력이 §c살의§f%만큼 증가합니다.",
+		"§7패시브 §8- §c살의§f: 근접 공격력이 §c살의§f%만큼 증가합니다.",
 		"§7패시브 §8- §5피의 향§f: 모든 플레이어의 현재 체력 비율을 볼 수 있습니다.",
 		" 근접 공격 시 대상의 체력이 33% 이하라면 §c살의§f를 $[MURDER_GAIN]만큼 획득합니다.",
 		" 만약 체력이 33%를 넘는다면 §c살의§f의 효과가 $[MULTIPLY]배가 되나 매번 §c살의§f를 $[MURDER_LOSS] 소모합니다.",
@@ -50,7 +56,7 @@ public class KillerBunny extends AbilityBase {
 
     public static final SettingObject<Double> MULTIPLY = 
             abilitySettings.new SettingObject<Double>(KillerBunny.class, "multiply", 1.2,
-            "# 살의 획득량") {
+            "# 기본 살의 효과 배수") {
 
         @Override
         public boolean condition(Double value) {
@@ -61,7 +67,7 @@ public class KillerBunny extends AbilityBase {
     
     public static final SettingObject<Double> SKILL_MULTIPLY = 
             abilitySettings.new SettingObject<Double>(KillerBunny.class, "skill-multiply", 2.0,
-            "# 스킬로 얻는 살의 획득량") {
+            "# 스킬 살의 효과 배수") {
 
         @Override
         public boolean condition(Double value) {
@@ -83,5 +89,31 @@ public class KillerBunny extends AbilityBase {
 	
     private final double murdergain = MURDER_GAIN.getValue();
     private final double murderloss = MURDER_LOSS.getValue();
+    private final double multiply = MULTIPLY.getValue();
+    private final double skillmultiply = SKILL_MULTIPLY.getValue();
+    private final int apparentdeath = (int) (APPARENTDEATH_DURATION.getValue() * 20);
+    
+    private double murder = 0;
+    
+    @SubscribeEvent
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+		Player damager = null;
+		if (e.getDamager() instanceof Projectile) {
+			Projectile projectile = (Projectile) e.getDamager();
+			if (projectile.getShooter() instanceof Player) damager = (Player) projectile.getShooter();
+		} else if (e.getDamager() instanceof Player) damager = (Player) e.getDamager();
+		
+		if (getPlayer().equals(damager) && e.getEntity() instanceof Player) {
+			Player player = (Player) e.getEntity();
+			double maxHP = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+			double power = (murder / 100);
+			if (player.getHealth() <= maxHP / 3.0) {
+				e.setDamage(e.getDamage() * (1 + power));
+			} else {
+				e.setDamage(e.getDamage() * (1 + (power * multiply)));
+			}
+		}
+    }
+    
     
 }
